c76770c8924e40251341d0befe59ba70e912decd
Kelly Hyeyeon Park <44021538+hy7721@users.noreply.github.com>
2021-08-20 17:51:55 +0900

    #add btn_top, select option style

--- js/m_main.js
+++ js/m_main.js
@@ -1,77 +1,74 @@
-// 2depth accordion menu
 $(document).ready(function() {
 
+  // 2depth accordion menu
   $(".list_q").click(function() {
     $(this).next(".list_a").stop().slideToggle(300);
     $(this).toggleClass('is_active').siblings().removeClass('is_active');
     $(this).next(".list_a").siblings(".list_a").slideUp(300);
+  });
+
+  $(".list_title").click(function() {
+    $(this).next(".list_article").stop().slideToggle(300);
+    $(this).toggleClass('is_active').siblings().removeClass('is_active');
+    $(this).next(".list_article").siblings(".list_a").slideUp(300);
+  });
+
+  // select option style
+  $('.select_option1').change(function() {
+    var current = $('.select_option1').val();
+    if (current != 'null') {
+      $('.select_option1').css('color','#333');
+    } else {
+      $('.select_option1').css('color','#CCC');
+    }
+  });
+
+  $('.select_option2').change(function() {
+    var current = $('.select_option2').val();
+    if (current != 'null') {
+      $('.select_option2').css('color','#333');
+    } else {
+      $('.select_option2').css('color','#CCC');
+    }
+  });
+
+  // btnTop
+  // IE not supports, IE 지원 X
+  window.scrollY;
+  document.scrollingElement.scrollTop;
+
+  // Supports all major browsers
+  document.documentElement.scrollTop;
+  document.querySelector('html').scrollTop;
+
+  // 스크롤이 끝으로 이동 시 이벤트 발생
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset <= 0) {
+      jQuery('#btnTop').removeClass('showTopbutton');
+    } else {
+      jQuery('#btnTop').addClass('showTopbutton');
+    }
+  });
+  jQuery(window).on("touchmove", function (e) {
+    if (window.pageYOffset <= 0) {
+      jQuery('#btnTop').removeClass('showTopbutton');
+    } else {
+      jQuery('#btnTop').addClass('showTopbutton');
+    }
+  });
+  jQuery('#btnTop').on('click touch', function() {
+    jQuery('html,body').animate({ scrollTop: 0 }, 300);
+    jQuery('#btnTop').removeClass('showTopbutton');
   });
 });
 
 // layer popup
 function layerPopupOpen($t){
   $(".wrap_popup" + "." + $t).stop(true, true).fadeIn(300);
+  // $('body').css("overflow", "hidden");
 };
 function layerPopupClose(){
   $(".wrap_popup").each(function(){
     if($(this).css("display") != "none") $(this).stop(true, true).fadeOut(300);
   });
 };
-
-
-
-(function() {
-  "use strict";
-
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
-
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
-
-  /**
-   * Back to top button
-   */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
-
-  /**
-   * Preloader
-   */
-  let preloader = select('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove()
-    });
-  }
-})()
