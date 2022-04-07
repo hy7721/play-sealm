07a3e71ace1538a1221c54b73d0b199d451c29fb
hy7721 <hykelly7721@gmail.com>
2022-04-07 14:22:35 +0900

    🔨 Add script to resize image

--- js/m_main.js
+++ js/m_main.js
@@ -1,52 +1,51 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
   // 2depth accordion menu
-  $(".list_q").click(function() {
-    $(this).next(".list_a").stop().slideToggle(300);
-    $(this).toggleClass('is_active').siblings().removeClass('is_active');
-    $(this).next(".list_a").siblings(".list_a").slideUp(300);
-  });
+  $('.list_q').click(function () {
+    $(this).next('.list_a').stop().slideToggle(300)
+    $(this).toggleClass('is_active').siblings().removeClass('is_active')
+    $(this).next('.list_a').siblings('.list_a').slideUp(300)
+  })
 
-  $(".list_title").click(function() {
-    $(this).next(".list_article").stop().slideToggle(300);
-    $(this).toggleClass('is_active').siblings().removeClass('is_active');
-    $(this).next(".list_article").siblings(".list_a").slideUp(300);
-  });
+  $('.list_title').click(function () {
+    $(this).next('.list_article').stop().slideToggle(300)
+    $(this).toggleClass('is_active').siblings().removeClass('is_active')
+    $(this).next('.list_article').siblings('.list_a').slideUp(300)
+  })
 
   // btnTop(IE not supports. Supports all major browsers)
-  window.scrollY;
-  document.scrollingElement.scrollTop;
-  document.documentElement.scrollTop;
-  document.querySelector('html').scrollTop;
+  window.scrollY
+  document.scrollingElement.scrollTop
+  document.documentElement.scrollTop
+  document.querySelector('html').scrollTop
 
   window.addEventListener('scroll', () => {
     if (window.pageYOffset <= 0) {
-      jQuery('#btnTop, #btnBottom').removeClass('showButton');
+      jQuery('#btnTop, #btnBottom').removeClass('showButton')
     } else {
-      jQuery('#btnTop, #btnBottom').addClass('showButton');
+      jQuery('#btnTop, #btnBottom').addClass('showButton')
     }
-  });
-  jQuery(window).on("touchmove", function (e) {
+  })
+  jQuery(window).on('touchmove', function (e) {
     if (window.pageYOffset <= 0) {
-      jQuery('#btnTop, #btnBottom').removeClass('showButton');
+      jQuery('#btnTop, #btnBottom').removeClass('showButton')
     } else {
-      jQuery('#btnTop, #btnBottom').addClass('showButton');
+      jQuery('#btnTop, #btnBottom').addClass('showButton')
     }
-  });
-  jQuery('#btnTop').on('click touch', function() {
-    jQuery('html,body').animate({ scrollTop: 0 }, 300);
-    jQuery('#btnTop').removeClass('showButton');
-  });
-  jQuery('#btnBottom').on('click touch', function() {
-    jQuery('html,body').animate({ scrollTop: $(document).height() }, 500);
-    jQuery('#btnBottom').removeClass('showButton');
-  });
+  })
+  jQuery('#btnTop').on('click touch', function () {
+    jQuery('html,body').animate({ scrollTop: 0 }, 300)
+    jQuery('#btnTop').removeClass('showButton')
+  })
+  jQuery('#btnBottom').on('click touch', function () {
+    jQuery('html,body').animate({ scrollTop: $(document).height() }, 500)
+    jQuery('#btnBottom').removeClass('showButton')
+  })
 
   // 모바일 100vh 하단 가려지는 현상 해결
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  let vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
   // document.querySelector('.wrap_result', '.wrap_receipt', '.wrap_error').style.height = window.innerHeight + "px";
-});
+})
 
 // loading
 // $(document).ready(function(){
@@ -58,29 +57,33 @@ $(document).ready(function() {
 // .ajaxStop(function(){
 //   $('#preloader').hide(); //ajax종료시 로딩바를 숨겨준다.
 // });
-$(window).on('load', function() {
+$(window).on('load', function () {
   if ($('#preloader').length) {
-    $('#preloader').delay(1).fadeOut('fast', function() {
-      $(this).remove();
-    });
+    $('#preloader')
+      .delay(1)
+      .fadeOut('fast', function () {
+        $(this).remove()
+      })
   }
-});
+})
 
 // layer popup
-function layerPopupOpen($t){
-  $(".wrap_popup" + "." + $t).stop(true, true).fadeIn(300);
+function layerPopupOpen($t) {
+  $('.wrap_popup' + '.' + $t)
+    .stop(true, true)
+    .fadeIn(300)
   // $('body').css("overflow", "hidden");
-};
-function layerPopupClose(){
-  $(".wrap_popup").each(function(){
-    if($(this).css("display") != "none") $(this).stop(true, true).fadeOut(300);
-  });
-};
+}
+function layerPopupClose() {
+  $('.wrap_popup').each(function () {
+    if ($(this).css('display') != 'none') $(this).stop(true, true).fadeOut(300)
+  })
+}
 
 // editor(summernote)
-$(document).ready(function() {
+$(document).ready(function () {
   $('.summernote').summernote({
-    placeholder: "내용을 입력해주세요.",
+    placeholder: '내용을 입력해주세요.',
     tabsize: 2,
     minHeight: 300,
     focus: true,
@@ -92,14 +95,14 @@ $(document).ready(function() {
       ['para', ['ul', 'ol', 'paragraph']],
       ['table', ['table']],
       ['insert', ['link', 'picture', 'video']],
-      ['view', ['codeview', 'help']]
+      ['view', ['codeview', 'help']],
     ],
 
     popover: {
       table: [
         ['add', ['addRowDown', 'addRowUp', 'addColLeft', 'addColRight']],
         ['delete', ['deleteRow', 'deleteCol', 'deleteTable']],
-        ['custom', ['tableStyles']]
+        ['custom', ['tableStyles']],
       ],
 
       air: [
@@ -107,22 +110,37 @@ $(document).ready(function() {
         ['font', ['bold', 'underline', 'clear']],
         ['para', ['ul', 'paragraph']],
         ['table', ['table']],
-        ['insert', ['link', 'picture']]
+        ['insert', ['link', 'picture']],
       ],
 
       image: [
         ['image', ['resizeFull', 'resizeHalf', 'resizeQuarter', 'resizeNone']],
         ['float', ['floatLeft', 'floatRight', 'floatNone']],
-        ['remove', ['removeMedia']]
+        ['remove', ['removeMedia']],
       ],
     },
 
-    // 2022.01.28 추가
+    callbacks: {
+      onImageUpload: function (image) {
+        let file = image[0]
+        let reader = new FileReader()
+        reader.onloadend = function () {
+          let image = $('<img>').attr('src', reader.result)
+          image.attr('width', '100%')
+          $('#summernote').summernote('insertNode', image[0])
+        }
+
+        reader.readAsDataURL(file)
+      },
+    },
+
     styleTags: [
-      'p', {
-        tag: 'p', className: 'txt_article'
+      'p',
+      {
+        tag: 'p',
+        className: 'txt_article',
       },
     ],
-    fontNames: ['Noto Sans KR', 'Apple SD Gothic Neo', 'sans-serif']
-  });
-});
+    fontNames: ['Noto Sans KR', 'Apple SD Gothic Neo', 'sans-serif'],
+  })
+})
